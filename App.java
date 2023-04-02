add03f24f8e938ea70e5f8b9fa326a8e8999d76a
Henrique Feijó <henrique.paim01@edu.pucrs.br>
2023-04-02 20:18:57 -0300

    Implementação exercício da mochila
    CoProgramming with Bruno Avellar

--- App.java
+++ App.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
+
 public class App {
 
     public static void main(String args[]) {
 
-        int valTeste = 4; //4 - 8 - 16 - 32
-        int[] vetTeste = new int[33];
+        //int valTeste = 4; //4 - 8 - 16 - 32
+        //int[] vetTeste = new int[33];
 
-        System.out.println(fiboRec(valTeste));
-        System.out.println(fibo(valTeste));
-        System.out.println(fiboMem(vetTeste, valTeste));
-        
+        //System.out.println(fiboRec(valTeste));
+        //System.out.println(fibo(valTeste));
+        //System.out.println(fiboMem(vetTeste, valTeste));
+        //knapsack();
+        System.out.println(knapsack(7));
 
     }
 
@@ -51,4 +54,65 @@ public class App {
         }
         return f[n];
     }
+
+    public static int knapsack(int capacidade) {
+        capacidade++;
+        ArrayList<Item> itens = new ArrayList<Item>();
+        iinicializarItens(itens);
+
+        int maxTab[][] = new int[itens.size() ][capacidade];
+        inicializaMochila(maxTab, capacidade, itens.size());
+
+        for(int i = 1; i<itens.size(); i++) {
+            for(int j = 1; j<capacidade; j++) {
+                maxTab[i][j] = itens.get(i).getPeso() <= j ?
+                        Math.max(maxTab[i-1][j],itens.get(i).getCusto() + maxTab[i-1][j - itens.get(i).getPeso()]) :
+                        maxTab[i-1][j];
+            }
+        }
+        return maxTab[itens.size()][capacidade-1];
+    }
+
+    private static void inicializaMochila(int[][] maxTab, int capacidade,int numLinhas) {
+        for(int i = 0; i<capacidade ; i++) {
+            maxTab[0][i] = 0;
+        }
+        for (int i=0; i<numLinhas; i++) {
+            maxTab[i][0] = 0;
+        }
+    }
+
+
+    public static void iinicializarItens(ArrayList<Item> itens) {
+        itens.add(null);
+        itens.add(new Item(5, 2));
+        itens.add(new Item(2, 4));
+        itens.add(new Item(2, 2));
+        itens.add(new Item(1, 3));
+    }
+
+    static class Item {
+       private  int peso;
+        private int custo;
+
+        public Item() {
+            peso = 0;
+            custo = 0;
+        }
+
+        public Item(int peso, int custo) {
+            this.peso = peso;
+            this.custo = custo;
+        }
+
+        public int getPeso() {
+            return peso;
+        }
+
+        public int getCusto() {
+            return custo;
+        }
+
+    }
 }
+
