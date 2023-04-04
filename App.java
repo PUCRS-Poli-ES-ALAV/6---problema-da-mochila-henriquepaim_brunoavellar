df5923b959b49d580ce8ef6a31d9ed3120e86a2a
Henrique Feijó <henrique.paim01@edu.pucrs.br>
2023-04-04 18:44:05 -0300

    Atualizacoes

--- App.java
+++ App.java
@@ -114,5 +114,21 @@ public class App {
         }
 
     }
+    Custos: Remoção=R, Inserção=I , Substituição=S e Match=M=0;m = tamanho[A];
+    n = tamanho[B];
+    matriz[0][0] = 0;
+    Para i = 1 até m
+    matriz[i][0] = matriz[i-1][0] + 1 // soma uma I;
+    Para j = 1 até n
+    matriz[0][j] = matriz[0][j-1] + 1 // Soma uma R;
+    Para i = 1 até m
+    Para j = 1 até n
+    Se A[i] == B[j]
+    custoExtra = 0 //Operação M;
+    Senão
+            custoExtra = 1 //Operação S;
+    matriz[i][j] = Mínimo(matriz[i-1][j] +1, matriz[i][j-1] +1,
+                          matriz[i-1][j-1] + custoExtra];
+    devolva matriz[m][n];
 }
 
